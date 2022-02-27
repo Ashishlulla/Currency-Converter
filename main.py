ece78b99f608ab745396bae2b8c209a51318ada8
Ashish Rajesh Lulla <lullaashish2807@gmail.com>
2022-02-28 00:02:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -184,6 +184,34 @@ def convert():
         elif currency2 == "US Dollar":
             total = amount*0.31
             total_var.set(total)
+    elif currency1 == "Kuwaiti Dinar":
+        if currency2 == "Afghani":
+            total = amount*302.41
+            total_var.set(total)
+        elif currency2 == "Canadian Dollar":
+            total = amount*4.21
+            total_var.set(total)
+        elif currency2 == "Chinese Yuan":
+            total = amount*20.90
+            total_var.set(total)
+        elif currency2 == "Euro":
+            total = amount*2.91
+            total_var.set(total)
+        elif currency2 == "Indian Rupee":
+            total = amount*246.83
+            total_var.set(total)
+        elif currency2 == "Israeli New Shekel":
+            total = amount*10.69
+            total_var.set(total)
+        elif currency2 == "Pakistani rupee":
+            total = amount*579.54
+            total_var.set(total)
+        elif currency2 =="Russian Rubble":
+            total = amount*262.52
+            total_var.set(total)
+        elif currency2 == "US Dollar":
+            total = amount*3.31
+            total_var.set(total)
             
 # -------------------- Labels ----------------------------
 
